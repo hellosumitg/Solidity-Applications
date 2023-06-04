85e2d181b21fd5a38fc433ee716dd7545663286e
Sumit Gupta <hellosumitg@yahoo.com>
2023-06-04 23:55:19 +0530

    Added two require statement in the code of Day4

--- ArrayElementReplaceFromEnd.sol
+++ ArrayElementReplaceFromEnd.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.18;
 
 contract ArrayElementReplaceFromEnd {
-     uint[] public arr;
-    
+    uint[] public arr;
+
     // "GAS EFFICIENT" way to remove an element from an array
     // Deleting an element creates a gap in the array.
     // One trick to keep the array compact is to
     // move the last element into the place to delete.
     function remove(uint index) public {
+        require(arr.length > 0, "Array is empty");
+        require(index < arr.length, "Invalid index");
+
         // Move the last element into the place to delete
         arr[index] = arr[arr.length - 1];
         // Remove the last element
@@ -21,7 +24,7 @@ contract ArrayElementReplaceFromEnd {
         remove(1); // to get [1, 4, 3]
 
         assert(arr.length == 3);
-         assert(arr[0] == 1);
+        assert(arr[0] == 1);
         assert(arr[1] == 4);
         assert(arr[2] == 3);
 
@@ -32,3 +35,4 @@ contract ArrayElementReplaceFromEnd {
         assert(arr[1] == 4);
     }
 }
+
