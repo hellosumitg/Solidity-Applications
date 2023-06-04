0a204fc2f642329e5041275194927eb0a97b3132
Sumit Gupta <hellosumitg@yahoo.com>
2023-06-04 17:45:33 +0530

    Day4

---
+++ ArrEleReplaceFromEnd.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract ArrayReplaceFromEnd {
+     uint[] public arr;
+    
+    // "GAS EFFICIENT" way to remove an element from an array
+    // Deleting an element creates a gap in the array.
+    // One trick to keep the array compact is to
+    // move the last element into the place to delete.
+    function remove(uint index) public {
+        // Move the last element into the place to delete
+        arr[index] = arr[arr.length - 1];
+        // Remove the last element
+        arr.pop();
+    }
+
+    function test() public {
+        arr = [1, 2, 3, 4];
+
+        remove(1); // to get [1, 4, 3]
+
+        assert(arr.length == 3);
+         assert(arr[0] == 1);
+        assert(arr[1] == 4);
+        assert(arr[2] == 3);
+
+        remove(2);
+        // [1, 4]
+        assert(arr.length == 2);
+        assert(arr[0] == 1);
+        assert(arr[1] == 4);
+    }
+}
